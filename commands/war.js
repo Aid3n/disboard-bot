317ac587e84e915920a6b30226d683171585e8e9
Aid3n <38618692+Aid3n@users.noreply.github.com>
2018-09-17 21:49:31 +0200

    Update war.js

--- commands/war.js
+++ commands/war.js
@@ -8,6 +8,7 @@ module.exports.run = async (bot, message, args) => {
     .addField("vs TR | 14/09/18", "459 | TR 525 | -66 Lose")
     .addField("vs If | 14/09/18", "339 | If 357 | -18 Lose")
     .addField("vs ID | 17/09/18", "477 | ID 487 | -10 Lose")
+    .addField("vs Hz | 17/09/18", "212 | Hz 256 | -44 Lose")
     .setFooter("Última actualización: 17/09/2018", "https://pbs.twimg.com/media/DnPeL62W0AYtW9Y.png:large")
     message.channel.send(embed);
 }
