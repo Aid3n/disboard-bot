6f457e80407a917f37c813f291834fd47c9b3f66
Aid3n <38618692+Aid3n@users.noreply.github.com>
2018-10-03 22:04:36 +0200

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -5,10 +5,11 @@ module.exports.run = async (bot, message, args) => {
     .setTitle("Estadísticas generales de Disboard")
     .setColor("#eef372")
     .setThumbnail("https://cdn.discordapp.com/attachments/490642615323328513/490977773637337099/487324012087869455.png")
-    .addField("Win:", "7", inline= true)
+    .addField("Win:", "8", inline= true)
     .addField("Lose:", "5", inline= true)
     .addField("Withdraw:", "1", inline= true)
-    .setFooter("Imanity: 16 Seirens: 3 Werebeast: 13  |  27/09/2018", "https://pbs.twimg.com/media/DnPeL62W0AYtW9Y.png:large")
+    .addField("Prop:", "69%", inline= true)
+    .setFooter("Imanity: 15 Seirens: 2 Werebeast: 9  |  27/09/2018", "https://pbs.twimg.com/media/DnPeL62W0AYtW9Y.png:large")
     message.channel.send(embed);
 }
 
